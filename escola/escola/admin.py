8a85f1bf77d0ba7de6e53b76b74fb0523d76e97b
franciscof <franciscof@cetsp.com.br>
2024-11-30 00:33:01 -0300

    Implementa a aula Incluindo páginação, ordenação e filtros

--- escola/escola/admin.py
+++ escola/escola/admin.py
@@ -7,14 +7,15 @@ class Estudantes(admin.ModelAdmin):
     list_display = ('id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular')
     list_display_links = ('id', 'nome',)
     list_per_page = 20
-    search_fields = ('nome',)
+    search_fields = ('nome','cpf')
+    ordering = ('nome',)
 
 admin.site.register(Estudante, Estudantes)
 
 class Cursos(admin.ModelAdmin):
     list_display = ('id', 'codigo', 'descricao')
     list_display_links = ('id', 'codigo',)
-    search_fields = ('codigo',)    
+    search_fields = ('codigo',)
 
 admin.site.register(Curso, Cursos)
 
--- escola/escola/views.py
+++ escola/escola/views.py
@@ -1,14 +1,18 @@
 from escola.models import Estudante, Curso, Matricula
 from escola.serializers import EstudanteSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasEstudanteSerializer, ListaMatriculasCursoSerializer
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, filters
 #from rest_framework.authentication import BasicAuthentication
 #from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework  import DjangoFilterBackend
 
 class EstudanteViewSet(viewsets.ModelViewSet):
     #authentication_classes = [BasicAuthentication]
     #permission_classes = [IsAuthenticated]
     queryset = Estudante.objects.all()
     serializer_class = EstudanteSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome', 'cpf']
 
 class CursoViewSet(viewsets.ModelViewSet):
     #authentication_classes = [BasicAuthentication]
